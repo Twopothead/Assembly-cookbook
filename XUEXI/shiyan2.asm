317d642041f1a5f4f2ac3adfde7c32e8fac337cb
Twopothead <3103204417@qq.com>
2017-01-18 12:59:25 +0800

    1+2+ +100=5050
    sum :5050

--- XUEXI/shiyan2.asm
+++ XUEXI/shiyan2.asm
@@ -4,42 +4,54 @@ data ends
 extra segment
  	dest dw 100 dup(0)
 extra ends
-assume cs:code,ds:data,es:extra 
+stack segment 
+   db 30 dup(0)
+stack ends
+assume cs:code,ds:data,es:extra,ss:stack 
 code segment
-start : mov ax,data
+start : mov ax,stack
+        mov ss,ax
+        mov sp,30 
+        mov ax,data
         mov ds,ax
-        mov ax,1
+       mov ax,1
         mov bx,0
         mov cx,100
      s: mov word ptr ds:[bx],ax
         inc ax
         add bx,2
         loop s
-        nop
-        nop
-copy: lea si,source
- 	  lea di,dest
- 	  cld ;方向标志清0
- 	  mov cx,100*2
- 	  rep movsb ;以字节形式传送cx次
- 	  nop
- 	  nop
- 	  lea si,source 
- 	  lea di,dest
- 	  cld 
- 	  mov cx,100*2
- 	  rep cmpsb
- 	  jz ok
- 	  jmp copy
- ok : lea di,dest
+      lea si ,source
+      mov ax,112  ;改为075a:0070;075a:0000里似乎有重要东西
+      mov di,ax
+      cld 
+      mov cx,100*2
+      rep movsb
+      nop
+      nop
+      mov ax,112
+      mov di,ax
       mov ax,0
-   	  mov cx,100
-plus: add ax,es:[di]
-   	  add di,2
-   	  loop plus
-	  mov ax,ax  
- 	  nop
-	  nop
+      mov cx,100
+ plus:     add ax,es:[di]
+      add di,2
+      loop plus
+      mov dx,0
+      mov bx,10
+      div bx;;;;;;ax=01f9(商505) dx=0(0)
+      push dx
+      div bx;;;;;;ax=0032(商50)  dx=5(余5)
+      push dx
+      div bx;;;;;;ax=0005(商5)  dx=0(余0)
+      push dx
+      push ax
+      nop 
+      mov cx,4
+show:pop dx
+  add dx ,30h
+   mov ax,0200h
+   int 21h
+   loop show
    mov ax,4c00h
    int 21h
 code ends
