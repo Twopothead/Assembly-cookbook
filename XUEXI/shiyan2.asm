6ee0456366069129486132df3615e72cb51dfe60
Twopothead <3103204417@qq.com>
2017-01-18 16:02:32 +0800

    076a:0000=075a:0000
    075a:0100=076a:0000
    076a:0000到076a:0036之间似乎有重要数据
    把本代码中的mov ax,37h改为mov ax,36h将会破坏它，从而屏幕无显示

--- XUEXI/shiyan2.asm
+++ XUEXI/shiyan2.asm
@@ -1,8 +1,8 @@
 data segment 
- 	source dw 100 dup(0)
+  source dw 100 dup(0)
 data ends
 extra segment
- 	dest dw 100 dup(0)
+  dest dw 100 dup(0)
 extra ends
 stack segment 
    db 30 dup(0)
@@ -22,17 +22,20 @@ start : mov ax,stack
         add bx,2
         loop s
       lea si ,source
-      mov ax,112  ;改为075a:0070;075a:0000里似乎有重要东西
+      mov ax,37h  ;改为075a:0037;075a:0000里似乎有重要东西
+      ;(这一行如果是mov ax,36h都将破坏重要数据，屏幕无显示)
       mov di,ax
       cld 
       mov cx,100*2
       rep movsb
       nop
       nop
-      mov ax,112
+      mov ax,37h
       mov di,ax
       mov ax,0
       mov cx,100
+      nop
+      nop
  plus:     add ax,es:[di]
       add di,2
       loop plus
