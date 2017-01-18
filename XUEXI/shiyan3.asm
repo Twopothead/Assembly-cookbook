a195cba5af6f1de6eff73186206e5eebe194d00e
Twopothead <3103204417@qq.com>
2017-01-19 00:37:46 +0800

    两位数
    19-6+5=18

--- XUEXI/shiyan3.asm
+++ XUEXI/shiyan3.asm
@@ -17,9 +17,45 @@ jns ok
 neg ax
 ok:
 ret 
+input :
+push bx
+mov ah,01h
+int 21h
+aaa
+mov bl,al
+int 21h
+aaa
+
+mov ah,0
+mov bh,0
+mov cx,10
+shi:add ax,bx
+loop shi
+;mov ax,bx
+pop bx
+mov word ptr[bx],ax
+nop
+ret 
 start:
+
+
+
 mov ax,data
 mov ds,ax
+lea bx,x
+call input
+mov ax,0200h
+mov dx,word ptr[bx]
+add dx,30h
+int 21h
+
+lea bx,z
+call input
+mov ax,0200h
+mov dx,word ptr[bx]
+add dx,30h
+int 21h
+
 mov ax,y
 call absolute 
 mov bx,ax
@@ -32,8 +68,18 @@ sub ax,bx
 add ax,bp
 mov dx,ax
 nop
-mov ax,0200h
-add dx,30h
+mov bp,10
+mov ax,dx
+mov bx,1
+imul bx
+aam
+add ax,3030h
+mov bx,ax
+mov dx,0
+mov dl,bh
+mov ah,2
+int 21h
+mov dl,bl
 int 21h
 mov ax,4c00h
 int 21h
