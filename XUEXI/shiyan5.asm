bea8443d7c47270596d9f4aa9d233e92480d5d04
Twopothead <3103204417@qq.com>
2017-01-23 21:54:45 +0800

    初步完成
    已经完成排序输出任务 ，接下来要改进一下输入方式

--- XUEXI/shiyan5.asm
+++ XUEXI/shiyan5.asm
@@ -1,12 +1,12 @@
 data segment
-buf db  80,?,40 dup('$')
-score db 80 dup('$')
-x dw 0
-msg db 'The score between 90 and 100:',0ah,0dh,'$'
-minmsg db 'Min is :',0ah,0dh,'$'
-maxmsg db 'Max is :',0ah,0dh,'$'
-midmsg db 'The middle score is: ',0ah,0dh,'$'
-rankmsg db 'Rank ordering:',0ah,0dh,'$'
+	buf   db  80,?,80 dup('$')
+	score db  80 dup('$')
+	x     dw  0
+ msg 	db 'The score between 90 and 100:',0ah,0dh,'$'
+ minmsg db 'Min is :',0ah,0dh,'$'
+ maxmsg db 'Max is :',0ah,0dh,'$'
+ midmsg db 'The middle score is:',0ah,0dh,'$'
+ rankmsg 	db 'Rank ordering:',0ah,0dh,'$'
 data ends
 stack segment
    db 16 dup(0)
@@ -14,68 +14,75 @@ stack ends
 assume cs:code,ds:data,ss:stack
 code segment
 huanhang:
-mov ah,02h
-mov dl,0ah
-int 21h
-ret
+	mov ah,02h
+	mov dl,0ah
+	int 21h
+	ret
 showstring:
-mov ah,09h
-int 21h
-ret
+	mov ah,09h
+	int 21h
+	ret
 transf:
-push cx
-mov cx,2
-
-mov x,0
-getnum:
-mov al,[si]
-and ax,000fh
-xchg ax,x
-mov dx,10
-mul dx
-add x,ax
-inc si
-loop getnum
-mov ax,x
-mov word ptr score[di],ax
-mov x,0
-add di,2
-
-pop cx
-ret
+   push cx
+	mov x,0
+	mov cx,2
+	getnum:
+		mov al,[si]
+		and ax,000fh
+		xchg ax,x
+		mov dx,10
+		mul dx
+		add x,ax
+		inc si
+	loop getnum
+	mov ax,x
+	mov word ptr score[di],ax
+	mov x,0
+	add di,2
+   pop cx
+ ret
 xianshi:
-
-mov cx,20
-mov di,0
-show:
-mov ah,2
-mov bx,word ptr score[di]
-mov dl,bl
-add dl,30h
-int 21h
-add di,2
-loop show
-call huanhang
-ret
+	mov cx,20
+	mov di,0
+	show:
+		mov ah,2
+		mov bx,word ptr score[di]
+		mov dl,bl
+		add dl,30h
+		int 21h
+		add di,2
+		loop show
+		call huanhang
+	ret
 huanchong:
-mov ah,9
-lea dx,buf+2
-int 21h
-
-ret
+		mov ah,9
+		lea dx,buf+2
+		int 21h
+		ret
 bin2dec:
-mov ax,bp
-mov bl,1
-imul bl
-aam
-add ax,3030h
-mov bx,ax
+		mov ax,bp
+		mov bl,1
+		imul bl
+		aam
+		add ax,3030h
+		mov bx,ax
+		mov ah,2
+		mov dl,bh
+		int 21h
+		mov dl,bl
+		int 21h
+		ret
+zhanshi:
+mov cx,11
+mov si,0
+xx:mov bp,word ptr score[si]
+call bin2dec
 mov ah,2
-mov dl,bh
+mov dl,20h
 int 21h
-mov dl,bl
-int 21h
-ret
+add si,2
+loop xx
+ret  
 start:
 ;mov ax,stack
 ;mov ss,ax
@@ -85,15 +92,11 @@ mov ds,ax
 mov  dx, offset buf
 mov ah,10
 int 21h
+mov ah,2
+mov dl,0dh
+int 21h
 
-      ;MOV AH,2
-      ;mov dl,0dh
-      ;INT 21H
 call huanhang
-
-
-
-
 
 lea si,buf
 add si,2
@@ -103,45 +106,40 @@ shu:call transf
 loop shu
 exit:
 
-
 mov bx,0
 mov cx,10;循环次数：11-1=10（次）
 daxunhuan:mov dx,cx
           mov di,0
-       xiaos:
-       mov bp,word ptr score[di]
-       add di,2
-       cmp bp,word ptr score[di]
-       jna zhenchang
-       xchg bp,word ptr score[di]
-     zhenchang:
-     mov word ptr score[di-2],bp
-     loop xiaos
-     mov cx,dx
-     loop daxunhuan
-     nop
-     nop  
-;call huiche
-;call xianshi
-;call huiche
-;call xianshi
+     	  xiaos:
+      			 mov bp,word ptr score[di]
+     			 add di,2
+   			     cmp bp,word ptr score[di]
+       			 jna zhenchang
+     		    xchg bp,word ptr score[di]
+     			zhenchang:
+    			 mov word ptr score[di-2],bp
+   			  loop xiaos
+    	 mov cx,dx
+    	 loop daxunhuan
+    	 nop
+    	 nop  
+
+	mov di,0
+	mov dx,offset msg
+	call showstring
+	;call xianshi
 
 
 
+	call huanhang
+	mov ah,2
+	mov dl,0dh
+	int 21h
+	mov ah,9
+	mov dx,offset minmsg
+	int 21h
 
-
-
-mov di,0
-
-mov dx,offset msg
-call showstring
-mov dx,offset minmsg
-call showstring
-;mov ah,2
 mov bx,word ptr score[0]
-;mov dl,bl
-;add dl,30h
-int 21h
 mov bp,bx
 call bin2dec
 call huanhang
@@ -149,34 +147,23 @@ call huanhang
 
 mov dx,offset maxmsg
 call showstring
-;mov ah,2
 mov bx,word ptr score[10*2]
-;mov dl,bh
-;add dl,30h
-;int 21h
-;mov dl,bl
-;add dl,30h
-;int 21h
 mov bp,bx
 call bin2dec
 call huanhang
 
 mov dx,offset midmsg
 call showstring
-;mov ah,2
 mov bx,word ptr score[5*2]
-;mov dl,bl
-;add dl,30h
-;int 21h
 mov bp,bx
 call bin2dec
 call huanhang
 
 mov dx,offset rankmsg
 call showstring
-call xianshi
-
+call zhanshi
 mov ax,4c00h
 int 21h
+
 code ends
 end start
